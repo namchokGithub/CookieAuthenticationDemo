3aa154aebdf1a7345079358ab6169a1830b9bab8
Namchok <pech4751@gmail.com>
2020-09-21 16:27:59 +0700

    init first commit

---
+++ CookieAuthenticationDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CookieAuthenticationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId
+        {
+            get; set;
+        }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
